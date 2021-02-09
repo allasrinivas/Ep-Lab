e75f38b28a15cacfa4778b77b156456529ceda1f
allasrinivas <74001095+allasrinivas@users.noreply.github.com>
2021-02-09 10:56:20 +0530

    Create RegBean.java

---
+++ 1-02-2021/RegBean.java
@@ -0,0 +1,43 @@
+public class RegBean {
+	
+	private int regno,year,sec;
+	private String name,email,pswd;
+	public int getRegno() {
+		return regno;
+	}
+	public void setRegno(int regno) {
+		this.regno = regno;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getSec() {
+		return sec;
+	}
+	public void setSec(int sec) {
+		this.sec = sec;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPswd() {
+		return pswd;
+	}
+	public void setPswd(String pswd) {
+		this.pswd = pswd;
+	}
+	
+	
+}
