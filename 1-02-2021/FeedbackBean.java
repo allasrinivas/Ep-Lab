a10464c21064dd78ffae3ec2c51d460afca326cc
allasrinivas <74001095+allasrinivas@users.noreply.github.com>
2021-02-09 10:51:14 +0530

    Create FeedbackBean.java

---
+++ 1-02-2021/FeedbackBean.java
@@ -0,0 +1,45 @@
+public class FeedbackBean {
+	
+	private String r1,r2,r3,r4,r5;
+
+	public String getR1() {
+		return r1;
+	}
+
+	public void setR1(String r1) {
+		this.r1 = r1;
+	}
+
+	public String getR2() {
+		return r2;
+	}
+
+	public void setR2(String r2) {
+		this.r2 = r2;
+	}
+
+	public String getR3() {
+		return r3;
+	}
+
+	public void setR3(String r3) {
+		this.r3 = r3;
+	}
+
+	public String getR4() {
+		return r4;
+	}
+
+	public void setR4(String r4) {
+		this.r4 = r4;
+	}
+
+	public String getR5() {
+		return r5;
+	}
+
+	public void setR5(String r5) {
+		this.r5 = r5;
+	}
+	
+}
