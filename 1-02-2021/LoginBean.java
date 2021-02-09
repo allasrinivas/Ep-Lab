36fc423d77559080a5088497706b30e161bb8a6a
allasrinivas <74001095+allasrinivas@users.noreply.github.com>
2021-02-09 10:55:37 +0530

    Create LoginBean.java

---
+++ 1-02-2021/LoginBean.java
@@ -0,0 +1,21 @@
+public class LoginBean {
+
+	private String email,pswd;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPswd() {
+		return pswd;
+	}
+
+	public void setPswd(String pswd) {
+		this.pswd = pswd;
+	}
+	
+}
